381e0f9ec6e8a0950034c67ecb7f6a83d1b48bdc
Yared57 <157310614+Yared57@users.noreply.github.com>
2025-04-26 06:30:04 -0700

    Create Countsort.py

---
+++ Countsort.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    Max=float('-inf')
+    for i in arr:
+        if(i>Max):
+            Max=i      
+    list1=[0]*100
+    for i in arr:
+        list1[i]+=1
+    return list1   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
