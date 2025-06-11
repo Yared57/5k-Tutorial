d7c222bdb982ce737b45f128fac186748e126afd
Yared57 <157310614+Yared57@users.noreply.github.com>
2025-06-11 04:16:19 -0700

    Update findmaxaverage.py

--- findmaxaverage.py
+++ findmaxaverage.py
@@ -1,13 +1,18 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        sums=0
         Max=float('-inf')
-        for i in range(k):
+        NK=k-1
+        sums=0
+        for i in range(0,k,1):
             sums+=nums[i]
-            Max = sums
-
-        for i in range(1,len(nums)-k+1):
-            sums=sums-nums[i-1]+nums[i+k-1]
-            if(sums>Max):
+            Max=sums
+        left=0
+        print(sums)   
+        for right in range(NK+1,len(nums),1):
+            sums-=nums[left]
+            sums+=nums[right]
+            left+=1 
+            if sums>Max:
                 Max=sums
-        return Max/k   
+               
+        return Max/k  
