45bcd32fab5a9ae77dbb38d2c8f076eef43285c2
Yared57 <157310614+Yared57@users.noreply.github.com>
2025-07-04 04:49:33 -0700

    Update Toeplitzmatrix.py

--- Toeplitzmatrix.py
+++ Toeplitzmatrix.py
@@ -1,12 +1,25 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        x=len(matrix)
-        acc=None
-        for i in range(x):
-            if(acc is None):
-                acc=matrix[i][i]
-            elif(acc!=matrix[i][i]):
-                return False
-            else:
-                acc=matrix[i][i]
-        return True    
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        
+        for i in range(cols):
+            Acc = matrix[0][i]
+            r, c = 0, i
+            while r < rows and c < cols:
+                if matrix[r][c] != Acc:
+                    return False
+                r += 1
+                c += 1
+
+        for i in range(1, rows):
+            Acc = matrix[i][0]
+            r, c = i, 0
+            while r < rows and c < cols:
+                if matrix[r][c] != Acc:
+                    return False
+                r += 1
+                c += 1
+
+        return True
